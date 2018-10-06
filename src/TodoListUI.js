9016a60cac05027f81bdb7ed6c5b56a28fc576b7
ChenYongYang <1214137301@qq.com>
2018-10-06 17:37:51 +0800

    无状态组件

--- src/TodoListUI.js
+++ src/TodoListUI.js
@@ -2,35 +2,63 @@ import React, { Component } from 'react';
 import { Input, Button, List } from 'antd';
 import 'antd/dist/antd.css'
 
-class TodoListUI extends Component{
-    render(){
-        return (
-            <div style={{ margin: '10px' }}>
-                <Input
-                    value={this.props.inputValue}
-                    placeholder="请输入内容"
-                    style={{ width: '300px', marginRight: '10px' }}
-                    onChange={this.props.handleInputChange}
-                />
-                <Button
-                    type="primary"
-                    onClick={this.props.handleButtonClick}
-                >
-                    提交
-                </Button>
-                <List
-                    style={{ marginTop: '10px', width: '300px' }}
-                    header={<h3>TodoList</h3>}
-                    bordered
-                    dataSource={this.props.list}
-                    // onclick触发的只是函数体，并能传递参数
-                    renderItem={(item, index) => (<List.Item onClick={() => {
-                        this.props.handleItemDelete(index);
-                    }}>{item}</List.Item>)}
-                />
-            </div>
-        )
-    }
+// 无状态组件
+const TodoListUI = (props) => {
+    return(
+        <div style={{ margin: '10px' }}>
+            <Input
+                value={props.inputValue}
+                placeholder="请输入内容"
+                style={{ width: '300px', marginRight: '10px' }}
+                onChange={props.handleInputChange}
+            />
+            <Button
+                type="primary"
+                onClick={props.handleButtonClick}
+            >提交</Button>
+            <List
+                style={{ marginTop: '10px', width: '300px' }}
+                header={<h3>TodoList</h3>}
+                bordered
+                dataSource={props.list}
+                // onclick触发的只是函数体，并能传递参数
+                renderItem={(item, index) => (<List.Item onClick={() => {
+                    props.handleItemDelete(index);
+                }}>{item}</List.Item>)}/>
+        </div>
+    )
 }
+
+
+// class TodoListUI extends Component{
+//     render(){
+//         return (
+//             <div style={{ margin: '10px' }}>
+//                 <Input
+//                     value={this.props.inputValue}
+//                     placeholder="请输入内容"
+//                     style={{ width: '300px', marginRight: '10px' }}
+//                     onChange={this.props.handleInputChange}
+//                 />
+//                 <Button
+//                     type="primary"
+//                     onClick={this.props.handleButtonClick}
+//                 >
+//                     提交
+//                 </Button>
+//                 <List
+//                     style={{ marginTop: '10px', width: '300px' }}
+//                     header={<h3>TodoList</h3>}
+//                     bordered
+//                     dataSource={this.props.list}
+//                     // onclick触发的只是函数体，并能传递参数
+//                     renderItem={(item, index) => (<List.Item onClick={() => {
+//                         this.props.handleItemDelete(index);
+//                     }}>{item}</List.Item>)}
+//                 />
+//             </div>
+//         )
+//     }
+// }
 
 export default TodoListUI;
